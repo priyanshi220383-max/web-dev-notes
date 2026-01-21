ab1c5eec9459c61ed26d6088e998cc1e20cf124a
User <priyanshi220383@gmail.com>
2026-01-21 14:31:13 +0530

    Added the folder DAY-5 and .gitignore file.

---
+++ DAY-5/script.js
@@ -0,0 +1,17 @@
+// let str = "JavaScript is fun!";
+// let sliced = str.slice(0, 10);
+// console.log(sliced)
+let str = "JavaScript is amazing!";
+let sub = str.substring(-2, 17);
+ console.log(sub)
+//  substring accept negative values whereas slice does not , slice also counts space 
+let sentence = "I,am,a,comma,separated,sentence";
+let words = sentence.split("a");
+console.log(words)
+// Find out the accurance of "r" in the given string
+let str1 = "tu meri mein tera mein teri tu mera"
+let sub1 = str1.split("r")
+let x = sub1.length-1
+// 
+console.log(sub1)
+console.log(x)
