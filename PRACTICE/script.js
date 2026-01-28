e752772b82480e08926435212782b7248a373426
User <priyanshi220383@gmail.com>
2026-01-28 19:11:19 +0530

    added my practice file

---
+++ PRACTICE/script.js
@@ -0,0 +1,14 @@
+// let a = 5;
+// let b = 10;
+// a++
+// b--
+// console.log("a =",a)
+// console.log("b =",b)
+function bro() {
+    console.log("welcome to my college");
+    console.log("welcome to my school");  
+    console.log("welcome to my house");
+    console.log("welcome to my place");
+}
+bro();
+bro();
